cd65eb62db1729aacf59d4ddb050d4412ccaced7
santiago <davids.guerrero@udea.edu.co>
2022-08-29 10:39:44 -0500

    Primeros

--- src/main/java/app/retos/models/Contacts.java
+++ src/main/java/app/retos/models/Contacts.java
@@ -1,2 +1,44 @@
-package app.retos.models;public class Contacts {
+package app.retos.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Document(collection = "contacts")
+@Data
+@NoArgsConstructor
+public class Contacts {
+
+    @Id
+    @JsonIgnore
+    private String id;
+
+    @JsonIgnore
+    @NotNull(message = "Username no puede ser nulo")
+    private String username;
+
+    @Pattern(regexp = "[A-Za-z]+", message = "Solo letras")
+    private String name;
+
+    @Pattern(regexp = "[A-Za-z]+", message = "Solo letras")
+    private String lastName;
+
+    @NotNull(message = "Email no puede ser nulo")
+    @Size(max = 50, message = "Tamaño incorrecto")
+    @Email(message = "Debe ser un email valido")
+    private String email;
+
+    @NotNull(message = "Numero de celular no puede ser nulo")
+    @Pattern(regexp = "[0-9]+", message = "Solo numeros")
+    @Size(min = 8, max = 12, message = "Tamaño de celular es incorrecto")
+    private String cellPhone;
+
 }
--- src/main/java/app/retos/models/Roles.java
+++ src/main/java/app/retos/models/Roles.java
@@ -1,18 +1,19 @@
-package com.app.usuarios.models;
+package app.retos.models;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "roles")
+@Data
+@NoArgsConstructor
 public class Roles {
 
 	@Id
 	private String id;
 
 	private String name;
-
-	public Roles() {
-	}
 
 	public Roles(String id, String name) {
 		super();
@@ -24,19 +25,4 @@ public class Roles {
 		this.name = name;
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
